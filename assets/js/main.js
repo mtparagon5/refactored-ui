7c925ed13ae461b7d267b356d3c5f329f51b820b
Matthew <mt.paragon5@gmail.com>
2020-01-01 23:19:13 -0500

    finished cards; need to add option to get variables;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 const palettes = ruiPalettes.Palettes;
 const swatches = ruiPalettes.Swatches;
 
-let paletteColors = []
-
-// console.log(palettes)
 palettes.forEach((palette) => {
   let colors = {}
   colors.paletteId = palette.id;
@@ -12,36 +9,55 @@ palettes.forEach((palette) => {
   });
   palette.colors = colors;
 });
-// console.log(palettes)
-
 
 $.map(palettes, function (palette) {
   let colorString = '<table class="table table-striped">';
   palette.colors.colors.forEach((color) => {
 
-    // console.log(color.shades.filter((shade) => { return shade.weight == "500" })[0].hexColor)
-
-    // color.shades.forEach((shade) => {
-    // console.log(shade)
     let hexColor = color.shades.filter((shade) => { return shade.weight == "300" })[0].hexColor;
     colorString += '<tr><td><span style="position:relative;top:5px;">' + color.color + '</span></td><td><span style="position:relative;top:5px;">' + hexColor + '</span></td><td><button class="btn btn-sm btn-just-icon" style="width:5px;background-color:' + hexColor + ';border-color:' + hexColor + '"></button></td></tr>'
-    // })
   });
 
   $('#palette-cards').append(
     `<div class="col-lg-4 col-md-6 col-xs-12">
-      <div class="card" style="min-height:300px;">
-        <img class="card-img-top" src="./assets/img/` + palette.id + `.png" alt="palette sample">
+      <div class="card" style="min-height:565px;">
+        <img id="`+ palette.id + `" class="card-img-top" src="./assets/img/` + palette.id + `.png" alt="palette sample">
         <div class="card-body">
-          <h4 class="card-title">Palette ` + palette.id + `</h4>
-          <p class="card-text">Primary: ` + palette.primaryColors.join(' and ') + `.<br>Neutral: ` + palette.neutralColor + `.<br>Supporting: ` + palette.supportingColors.join(', ') + `.</p>
+          <h2 class="card-title text-center">Palette ` + palette.id + `</h2>
+
+          <h4 class="card-title text-center">Primary</h4>
+          <p class="card-text text-center">` + palette.primaryColors.join(' and ') + `</p>
+          
+          <h4 class="card-title text-center">Neutral</h4>
+          <p class="card-text text-center">` + palette.neutralColor + `</p>
+          
+          <h4 class="card-title text-center">Supporting</h4>
+          <p class="card-text text-center">` + palette.supportingColors.join(', ') + `</p>
+          
         </div>
-        <a href="#" class="btn btn-primary">Show Colors</a>
-        <div id="palette-colors" style="display:none;">
-          <h4>Palette Colors</h4>
+        <button id="colors` + palette.id + `" class="btn btn-primary col-12">Show Color Palette</button>
+        <div id="colors` + palette.id + `" style="display:none; ">
           ` + colorString + `
         </div>
       </div>
     </div>`
   )
 });
+
+$(document).ready(function () {
+
+  $('img').click((event) => {
+    $('div#colors' + event.target.id).slideToggle('slow');
+    $('button#colors' + event.target.id).text($('button#colors' + event.target.id).text() == 'Show Color Palette' ? 'Hide Color Palette' : 'Show Color Palette');
+    console.log(event.target.id)
+  })
+
+  $('button').click((event) => {
+    // show/hide colors div
+    $('div#' + event.target.id).slideToggle("slow");
+    // change button text
+    $(event.target).text($(event.target).text() == 'Show Color Palette' ? 'Hide Color Palette' : 'Show Color Palette');
+
+  });
+
+})
